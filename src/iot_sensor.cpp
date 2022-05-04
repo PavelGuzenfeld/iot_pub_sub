930ec14f21011c84a29b9645823821c4ba5133e6
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2022-05-04 18:16:58 +0300

    sensor refactor

--- src/iot_sensor.cpp
+++ src/iot_sensor.cpp
@@ -1,9 +1,5 @@
-#include <random>
-#include <time.h>
-#include <sstream>
 #include "iot_sensor.hpp"
-
-#include <iostream>
+#include "iot_utils.hpp"
 
 namespace iot
 {
@@ -15,45 +11,14 @@ Sensor::Sensor(Attributes const& a_att, Config const& a_conf)
 
 }
 
-static Time timestamp()
-{
-    time_t ptm = time(nullptr);
-    struct tm* curtime = localtime ( &ptm );
-    return Time(asctime(curtime));
-}
-
-static int getNextNumber(std::stringstream & a_str)
-{
-    int found = 0;
-    while (!a_str.eof())
-    {
-        String temp;
-        a_str >> temp;
-        if (std::stringstream(temp) >> found)
-        {
-            return found;
-        }
-    }
-    throw "bad config";
-}
-
-static int parse(String const& a_conf)
-{
-    std::stringstream ss;
-    ss << a_conf;   
-    return getNextNumber(ss);
-}
-
 static String eventData(String const& a_conf)
 {
-    std::random_device rd; // obtain a random number from hardware
-    std::mt19937 gen(rd()); // seed the generator
-
-    int from = parse(a_conf);
-    int to = parse(a_conf);
-    std::uniform_int_distribution<> distr(from, to); // define the range
-
-    return std::to_string(distr(gen));
+    int from;
+    nextNumber(a_conf, from);
+    int to;
+    nextNumber(a_conf, to);
+    auto data = randomUniformDistribution(from, to);
+    return std::to_string(data);
 }
 
 Event Sensor::produce()
